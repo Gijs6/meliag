f214b875ef256f7c55744bef2ee23f977410c438
Gijs <79806264+Gijs6@users.noreply.github.com>
2025-01-26 13:10:17 +0100

    This maybe works a little bit?

--- main.py
+++ main.py
@@ -25,23 +25,10 @@ def meliag_colofon():
 
 
 
+
+
 @app.route("/meliag/api/pos")
 def api_pos():
-    with open("kaart.pkl", "rb") as file:
-        data = pickle.load(file)
-
-    last_updated = data.get("last_updated", 0)
-    current_time = time.time()
-
-    if current_time - last_updated > 120:
-        threading.Thread(target=pos_data_bijwerken).start()
-
-    return jsonify(data.get("data",[]))
-
-
-
-
-def pos_data_bijwerken():
     url = "https://gateway.apiportal.ns.nl/virtual-train-api/vehicle?lat=0&lng=0"
     headers = {
         "Cache-Control": "no-cache",
@@ -57,43 +44,95 @@ def pos_data_bijwerken():
 
     data2 = []
 
+
+    with open("ritnummers.pkl", "rb") as file:
+        ritnummerData = pickle.load(file)
+
+
+    ritnummersDataVernieuwen = []
+
+
     for item in data["payload"]["treinen"]:
-        url = f"https://gateway.apiportal.ns.nl/virtual-train-api/v1/trein/{item['treinNummer']}?features=drukte"
-        headers = {
-            "Cache-Control": "no-cache",
-            "Ocp-Apim-Subscription-Key": api_key
-        }
-
-        response = requests.get(url, headers=headers)
-
-        if response.status_code != 200:
-            return 500
-
-        treindata = response.json()
-
         data2.append({
             "ritNum": item.get("treinNummer", "N/A"),
             "lat": item.get("lat", "N/A"),
             "lon": item.get("lng", "N/A"),
             "snelheid": item.get("snelheid", "N/A"),
             "treinSoort": item.get("type", "N/A"),
-            "materieel": treindata.get("type", "N/A"),
-            "vervoerder": treindata.get("vervoerder", "N/A"),
-            "station": treindata.get("station", "N/A"),
-            "spoor": treindata.get("spoor", "N/A"),
-            "materieelNums": [deel.get("materieelnummer", "N/A") for deel in treindata.get("materieeldelen", [])],
-            "afbeeldingen": [deel.get("afbeelding", "N/A") for deel in treindata.get("materieeldelen", [])]
         })
 
+        try:
+            dataDezeTrein = ritnummerData[str(item["treinNummer"])]
+            if (time.time() - dataDezeTrein["laatstBijgewerkt"]) > 300:
+                ritnummersDataVernieuwen.append(item["treinNummer"])
+        except KeyError:
+            ritnummersDataVernieuwen.append(item["treinNummer"])
 
-    datastuff = {
-        "last_updated": time.time(),
-        "data": data2
-    }
+
+    thread = threading.Thread(target=dataVernieuwenVanRitnummers, args=(ritnummersDataVernieuwen,))
+    thread.start()
 
 
-    with open("kaart.pkl", "wb") as file:
-            pickle.dump(datastuff, file)
+    return jsonify(data2)
+
+
+
+
+def dataVernieuwenVanRitnummers(ritnummerlijst):
+    with open("ritnummers.pkl", "rb") as file:
+        oudeData = pickle.load(file)
+
+    itemsVerwijderen = []
+
+    for ritnummer, itemdata in oudeData.items():
+        if (time.time() - itemdata.get("laatstBijgewerkt", 0)) > 600 or itemdata.get("ritnummer", "N/A") in ritnummerlijst:
+            itemsVerwijderen.append(itemdata.get("ritnummer", "N/A"))
+
+    for item in itemsVerwijderen:
+        if item in oudeData:
+            del oudeData[str(item)]
+
+    for ritnummer in ritnummerlijst:
+        try:
+            url = f"https://gateway.apiportal.ns.nl/virtual-train-api/v1/trein/{ritnummer}?features=drukte"
+
+            headers = {
+                "Cache-Control": "no-cache",
+                "Ocp-Apim-Subscription-Key": api_key
+            }
+
+            response = requests.get(url, headers=headers)
+
+            if response.status_code != 200:
+                return 500
+
+            treindata = response.json()
+
+            oudeData[str(ritnummer)] = ({
+                "ritnummer": ritnummer,
+                "laatstBijgewerkt": time.time(),
+                "materieel": treindata.get("type", "N/A"),
+                "vervoerder": treindata.get("vervoerder", "N/A"),
+                "station": treindata.get("station", "N/A"),
+                "spoor": treindata.get("spoor", "N/A"),
+                "materieelNums": [deel.get("materieelnummer", "N/A") for deel in treindata.get("materieeldelen", [])],
+                "afbeeldingen": [deel.get("afbeelding", "N/A") for deel in treindata.get("materieeldelen", [])]
+            })
+
+        except Exception as e:
+           print(f"Errort {e}")
+
+    with open("ritnummers.pkl", "wb") as file:
+        pickle.dump(oudeData, file)
+
+
+
+
+
+@app.route("/meliag/api/ritnummers")
+def api_ritnums():
+    with open("ritnummers.pkl", "rb") as file:
+        return jsonify(pickle.load(file))
 
 
 
